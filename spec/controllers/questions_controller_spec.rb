144f952ee5e9fa914dbe630a09479ef00579e762
Makeam <miel.design@gmail.com>
2015-06-04 15:33:12 +0300

    Created views. Guest can create question.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -69,7 +69,14 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
     context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, question: FactoryGirl.attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
 
+      it 're-renders new view' do
+        post :create, question: FactoryGirl.attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
     end
   end
 
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -4,4 +4,8 @@ FactoryGirl.define do
     body "MyTextMyText"
   end
 
+  factory :invalid_question, class: 'Question' do
+    title nil
+    body nil
+  end
 end
