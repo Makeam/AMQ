dd7c539ad5da9e4fd576a60691692b68a97b4d7e
Makeam <miel.design@gmail.com>
2015-08-16 05:01:21 +0300

    Refactor Attachments tests with shared examples

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -4,60 +4,17 @@ RSpec.describe AttachmentsController, type: :controller do
   describe "Remove attached files" do
     let!(:owner){ create(:user ) }
     let!(:user){ create(:user ) }
-    let!(:question){ create(:question, user: owner) }
-    let!(:answer){ create(:answer, user: owner) }
 
-    before do
-      question.attachments.create(attributes_for(:question_attachment))
-      answer.attachments.create(attributes_for(:answer_attachment))
+    context "Question" do
+      let!(:attachable){ create(:question, user: owner) }
+
+      it_behaves_like "Attachment destroy"
     end
 
-    context 'Question\'s owner can delete attached file', js: true do
-      it "should delete attachment" do
-        sign_in(owner)
-        expect{ delete :destroy, id: question.attachments.first.id, format: :js }.to change(question.attachments, :count).by(-1)
-      end
-      it "Response status: 200" do
-        sign_in(owner)
-        delete :destroy, id: question.attachments.first.id, format: :js
-        expect(response.status).to eq 200
-      end
-    end
+    context "Answer" do
+      let!(:attachable){ create(:answer, user: owner) }
 
-    context 'Not owner of question can\'t delete attached file', js: true do
-      it "should NOT delete attachment" do
-        sign_in(user)
-        expect{ delete :destroy, id: question.attachments.first.id, format: :js }.to change(question.attachments, :count).by(0)
-      end
-      it "Response status: 302" do
-        sign_in(user)
-        delete :destroy, id: question.attachments.first.id, format: :js
-        expect(response.status).to eq 302
-      end
-    end
-
-    context 'Answer\'s owner can delete attached file', js: true do
-      it "should delete attachment" do
-        sign_in(owner)
-        expect{ delete :destroy, id: answer.attachments.first.id, format: :js }.to change(answer.attachments, :count).by(-1)
-      end
-      it "Response status: 200" do
-        sign_in(owner)
-        delete :destroy, id: answer.attachments.first.id, format: :js
-        expect(response.status).to eq 200
-      end
-    end
-
-    context 'Not owner of answer can\'t delete attached file', js: true do
-      it "should NOT delete attachment" do
-        sign_in(user)
-        expect{ delete :destroy, id: answer.attachments.first.id, format: :js }.to change(answer.attachments, :count).by(0)
-      end
-      it "Response status: 302" do
-        sign_in(user)
-        delete :destroy, id: answer.attachments.first.id, format: :js
-        expect(response.status).to  eq 302
-      end
+      it_behaves_like "Attachment destroy"
     end
   end
 end
--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
 
+  factory :attachment, class:'Attachment' do
+    file ActionDispatch::Http::UploadedFile.new(tempfile: File.new("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
+    attachable { |a| a.association(:answer) }
+  end
+
   factory :question_attachment, class:'Attachment' do
     file ActionDispatch::Http::UploadedFile.new(tempfile: File.new("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
     attachable { |a| a.association(:question) }
